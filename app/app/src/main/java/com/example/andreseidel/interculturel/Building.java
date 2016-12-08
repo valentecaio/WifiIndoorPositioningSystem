1b803880a5e4ad04e71fcb455907f226b3f69682
aseidelo <aseideloliveira@gmail.com>
2016-12-08 18:09:17 +0100

    cbuilding added

---
+++ app/app/src/main/java/com/example/andreseidel/interculturel/Building.java
@@ -0,0 +1,15 @@
+package com.example.andreseidel.interculturel;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by AndreSeidel on 08/12/16.
+ */
+
+public class Building {
+    private String name;
+    private LatLng b1;
+    private LatLng b2;
+    private LatLng b3;
+    private LatLng b4;
+}
