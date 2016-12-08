628a334eda5cf5bdf7a70665e50f12b1a4bb3346
Caio Valente <caio-valente@hotmail.com>
2016-12-08 20:00:08 +0100

    add IO class

---
+++ app/app/src/main/java/com/example/andreseidel/interculturel/IO.java
@@ -0,0 +1,13 @@
+package com.example.andreseidel.interculturel;
+
+import android.util.Log;
+
+/**
+ * Created by caio on 08/12/2016.
+ */
+
+public class IO {
+    public static void print(String str){
+        Log.e("IO", str);
+    }
+}
