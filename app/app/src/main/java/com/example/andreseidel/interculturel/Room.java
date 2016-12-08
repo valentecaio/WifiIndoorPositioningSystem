b5693e32bfb1f02802caeffa88edeea06735ce12
aseidelo <aseideloliveira@gmail.com>
2016-12-08 08:35:03 +0100

    add app android first version

---
+++ app/app/src/main/java/com/example/andreseidel/interculturel/Room.java
@@ -0,0 +1,35 @@
+package com.example.andreseidel.interculturel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AndreSeidel on 07/12/16.
+ */
+
+public class Room {
+    private String name;
+    private List<RouterInRoom> routers;
+
+    public Room(String name){
+        this.name = name;
+        this.routers = new ArrayList<RouterInRoom>();
+    }
+
+    public void add(RouterInRoom rout){
+        for(RouterInRoom router : routers){
+            if (router.toString().equals(rout.toString())){
+                router.add(rout);
+                return;
+            }
+        }
+        routers.add(rout);
+    }
+
+    public toString(){
+        String str = "";
+        for (RouterInRoom router : routers){
+            str = str+router+toString();
+        }
+    }
+}
