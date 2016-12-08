8ab72518a18a7121174d498a246a4275a20c6301
aseidelo <aseideloliveira@gmail.com>
2016-12-08 19:51:06 +0100

    printing buildings and routes

--- app/app/src/main/java/com/example/andreseidel/interculturel/Building.java
+++ app/app/src/main/java/com/example/andreseidel/interculturel/Building.java
@@ -1,6 +1,9 @@
 package com.example.andreseidel.interculturel;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 /**
  * Created by AndreSeidel on 08/12/16.
@@ -8,8 +11,42 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class Building {
     private String name;
-    private LatLng b1;
-    private LatLng b2;
-    private LatLng b3;
-    private LatLng b4;
+    private PolygonOptions boarders;
+    private LatLng center;
+
+
+    public Building(String name, PolygonOptions b) {
+        this.name = name;
+        this.boarders = b;
+    }
+
+    public Building(String name, PolygonOptions boarders, LatLng center) {
+        this.name = name;
+        this.boarders = boarders;
+        this.center = center;
+    }
+
+    public LatLng getCenter() {
+        return center;
+    }
+
+    public void setCenter(LatLng center) {
+        this.center = center;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PolygonOptions getBoarders() {
+        return boarders;
+    }
+
+    public void setBoarders(PolygonOptions boarders) {
+        this.boarders = boarders;
+    }
 }
