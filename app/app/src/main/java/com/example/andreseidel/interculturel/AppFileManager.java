e15a8075b89a6e5da179beb8878cb161b3d2c107
Caio Valente <caio-valente@hotmail.com>
2016-12-11 21:54:06 +0100

    add function exportData

--- app/app/src/main/java/com/example/andreseidel/interculturel/AppFileManager.java
+++ app/app/src/main/java/com/example/andreseidel/interculturel/AppFileManager.java
@@ -1,11 +1,13 @@
 package com.example.andreseidel.interculturel;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,6 +15,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by caio on 08/12/2016.
@@ -45,9 +49,9 @@ public class AppFileManager {
         this.name = name;
     }
 
-    public String nameWithType(){
+    public String nameWithType() {
         String str = this.name;
-        if(!this.name.contains(this.fileType)) {
+        if (!this.name.contains(this.fileType)) {
             str += this.fileType;
         }
         return str;
@@ -62,23 +66,18 @@ public class AppFileManager {
             outputStreamWriter.write(data);
             outputStreamWriter.close();
             IO.print(this, "wrote with sucess the file " + this.name);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
     }
 
-    public ArrayList<String> readAll(){
-        // get paths from files
-        String path = this.context.getFilesDir().getAbsolutePath();
-        IO.print("Path: " + path);
-        File directory = new File(path);
-        File[] files = directory.listFiles();
+    public ArrayList<String> readAll() {
+        File[] files = this.getFiles();
 
         // read files
         ArrayList<String> results = new ArrayList<String>();
-        for(File file: files) {
-            if(formatIsCSV(file)) {
+        for (File file : files) {
+            if (formatIsCSV(file)) {
                 this.name = (file.getName());
                 results.add(read());
             }
@@ -86,15 +85,12 @@ public class AppFileManager {
         return results;
     }
 
-    public boolean deleteAll(){
-        String path = this.context.getFilesDir().getAbsolutePath();
-        IO.print("Path: " + path);
-        File directory = new File(path);
-        File[] files = directory.listFiles();
+    public boolean deleteAll() {
+        File[] files = this.getFiles();
 
         boolean deleted = true;
-        for(File file: files){
-            if(formatIsCSV(file)) {
+        for (File file : files) {
+            if (formatIsCSV(file)) {
                 IO.print("deleting " + file.getName());
                 deleted = file.delete() && deleted;
             }
@@ -102,16 +98,13 @@ public class AppFileManager {
         return deleted;
     }
 
-    public boolean deleteFile(){
-        String path = this.context.getFilesDir().getAbsolutePath();
-        IO.print("Path: " + path);
-        File directory = new File(path);
-        File[] files = directory.listFiles();
+    public boolean deleteFile() {
+        File[] files = this.getFiles();
 
         boolean deleted = true;
-        for(File file: files){
+        for (File file : files) {
             String nameToDelete = nameWithType();
-            if(nameToDelete.equals(file.getName()) && formatIsCSV(file)) {
+            if (nameToDelete.equals(file.getName()) && formatIsCSV(file)) {
                 IO.print("deleting " + file.getName());
                 deleted = file.delete();
             }
@@ -119,7 +112,7 @@ public class AppFileManager {
         return deleted;
     }
 
-    public boolean formatIsCSV(File file){
+    public boolean formatIsCSV(File file) {
         return file.getName().contains(fileType);
     }
 
@@ -130,21 +123,20 @@ public class AppFileManager {
             InputStream inputStream = context.openFileInput(nameToRead);
             IO.print("reading from " + nameToRead);
 
-            if ( inputStream != null ) {
+            if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 String receiveString = "";
                 StringBuilder stringBuilder = new StringBuilder();
 
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                while ((receiveString = bufferedReader.readLine()) != null) {
                     stringBuilder.append(receiveString);
                 }
 
                 inputStream.close();
                 ret = stringBuilder.toString();
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());
@@ -170,16 +162,67 @@ public class AppFileManager {
         return room;
     }
 
-    public ArrayList<Room> readAllRoomsAsArray(){
+    public ArrayList<Room> readAllRoomsAsArray() {
         // read all files
         ArrayList<String> roomsCSV = this.readAll();
 
         // instantiate all rooms
         ArrayList<Room> rooms = new ArrayList<Room>();
-        for(String roomCSV: roomsCSV){
+        for (String roomCSV : roomsCSV) {
             rooms.add(this.roomFromCSV(roomCSV));
         }
         return rooms;
+    }
+
+    public void exportAll(String outputFilename) throws IOException {
+        // gets the files to export
+        File[] files = this.getFiles();
+        ArrayList<String> srcFiles = new ArrayList<String>();
+        for (File file : files) {
+            if (formatIsCSV(file)) {
+                srcFiles.add(file.getAbsolutePath());
+            }
+        }
+
+        // generates zip file path
+        String zipFile = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS).getAbsolutePath()
+                + "/" + outputFilename + ".zip";
+        IO.print("exporting the files: " + srcFiles.toString());
+        IO.print("to the file: " + zipFile);
+
+        try {
+            // create byte buffer
+            byte[] buffer = new byte[1024];
+            FileOutputStream fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            for (int i = 0; i < srcFiles.size(); i++) {
+                File srcFile = new File(srcFiles.get(i));
+                FileInputStream fis = new FileInputStream(srcFile);
+                // begin writing a new ZIP entry, positions the stream to the start of the entry data
+                zos.putNextEntry(new ZipEntry(srcFile.getName()));
+
+                int length;
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
+            // close the ZipOutputStream
+            zos.close();
+        } catch (IOException ioe) {
+            IO.print("Error creating zip file: " + ioe);
+        }
+    }
+
+    private File[] getFiles(){
+        String path = this.context.getFilesDir().getAbsolutePath();
+        IO.print("Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        return files;
     }
 
 }
--- app/app/src/main/java/com/example/andreseidel/interculturel/InitialPageActivity.java
+++ app/app/src/main/java/com/example/andreseidel/interculturel/InitialPageActivity.java
@@ -1,11 +1,16 @@
 package com.example.andreseidel.interculturel;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.IOException;
 
 public class InitialPageActivity extends AppCompatActivity {
     EditText roomName;
@@ -36,6 +41,42 @@ public class InitialPageActivity extends AppCompatActivity {
     public void getMap(View view) {
         Intent intent = new Intent(this, TelecomMapActivity.class);
         startActivity(intent);
+    }
+
+    public void exportData(View view){
+        IO.print("export data");
+        AppFileManager fileMgr = new AppFileManager(this);
+
+        try {
+            askExportPermission();
+            fileMgr.exportAll("Interculturel");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void askExportPermission(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
 }
