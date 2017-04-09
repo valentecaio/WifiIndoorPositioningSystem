0805db62b42cd2253aec0ef9ba82f61ed4331591
Caio Valente <caio-valente@hotmail.com>
2017-04-09 16:06:46 +0200

    rm find-scripts

--- emploiDuTemps/getXpaths.py
+++
@@ -1,9 +0,0 @@
-import requests
-from lxml import html
-
-page = requests.get("https://www.facebook.com/")
-root = html.fromstring(page.text)
-tree = root.getroottree()
-result = root.xpath('//*[. = "XML"]')
-for r in result:
-	print(tree.getpath(r))
